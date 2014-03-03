67102aefc0bb1762ddad301353d9ac6b39806a59
valtercavalcante <valtinholima@gmail.com>
2014-03-03 16:31:46 -0300

    teste1

--- application/controllers/inicio.php
+++ application/controllers/inicio.php
@@ -12,15 +12,6 @@ class Inicio extends CI_Controller {
         $data['conteudo'] = 'painel/view_acesso';
         $this->load->view('tplCreate',$data);//tplCreate = Template padrao utilizado para cadastros
 	}
-	public function form1()
-	{
-		$data['legenda']= 'formulario exemplo';
-        $this->load->helper('form');
-        $this->load->helper('url');        
-        $data['js'] = load_js(array('jsCadastroUsuario.js'));//carrega o js para formatar os campos
-        $data['conteudo'] = 'usuario/formCadastro';
-        $this->load->view('tplCreate',$data);//tplCreate = Template padrao utilizado para cadastros
-	}
 	
 }
 
